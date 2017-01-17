7f9432736a37407b7466dc52b82d039697a72d0f
misk4 <misk4@naver.com>
2017-01-17 20:58:59 +0900

    final

--- BJ1005/BJ1005/BJ1005.cpp
+++ BJ1005/BJ1005/BJ1005.cpp
@@ -3,16 +3,35 @@
 
 using namespace std;
 
-int dfs() {
+int dfs(int s,int numOfRules,int *to,int *from,int *time,int *dp ) {
+	int result = 0,temp;
 
+	if (dp[s - 1] != -1) {
+		return dp[s - 1];
+	}
 
-	return 0;
+	for (int i = 0; i < numOfRules; i++) {
+		if (to[i] == s) {
+			temp = dfs(from[i], numOfRules, to, from, time, dp);
+			if (result < temp) {
+				result = temp;
+			}
+		}
+	}
+
+	result += time[s-1];
+	if (dp[s - 1] < result) {
+		dp[s - 1] = result;
+	}
+
+	return result;
 }
 
 int main() {
 	int trial,numOfBuildings,numOfRules;
 	int *result;
 	int time[1000];
+	int dp[1000];
 	int from[10000];
 	int to[10000];
 	int start;
@@ -30,7 +49,9 @@ int main() {
 		}
 		cin >> start;
 
-		result[i] = dfs();
+		fill_n(dp, 1000, -1);
+
+		result[i] = dfs(start,numOfRules,to,from,time,dp);
 	}
 
 	for (int i = 0; i < trial; i++) {
