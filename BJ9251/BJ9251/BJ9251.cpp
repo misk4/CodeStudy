4dd08e644f307d1d578d35c074efab39bc539b99
misk4 <misk4@naver.com>
2017-01-24 14:29:23 +0900

    bj9251

--- BJ9251/BJ9251/BJ9251.cpp
+++ BJ9251/BJ9251/BJ9251.cpp
@@ -1,6 +1,6 @@
-#include <iostream>
 #include <cstdio>
 #include <string>
+#include <iostream>
 
 using namespace std;
 
@@ -10,32 +10,46 @@ int max(int a, int b) {
 }
 
 int main() {
-	string s,s2;
-	cin >> s;
-	cin >> s2;
+	string s, s2;
+	cin >> s >> s2;
 
-	int dp[1002][1002];
-	
+	int result = 0, temp, x = s.length(), y = s2.length();
 
-	int result = 0;
+	int **dp = new int*[y + 2];
+	for (int i = 0; i < y + 2; i++) {
+		dp[i] = new int[x + 2];
+	}
+	for (int i = 0; i < y + 2;i++) {
+		dp[i][0] = 0;
+	}
+	for (int i = 0; i < x + 2; i++) {
+		dp[0][i] = 0;
+	}
 
-	for (int i = 1; i <= (int)s.length(); i++) {
-		for (int j = 1; j <= (int)s2.length(); j++) {
-			if (s[i] == s2[j]) {
-				dp[i][j] = dp[i - 1][j - 1] + 1;
+	for (int i = 1; i <=y ; i++) {
+		temp = 0;
+		for (int j = 1; j <= x; j++) {
+			if (s[j-1] == s2[i-1]) {
+				temp = dp[i - 1][j - 1] + 1;
+				dp[i][j] = temp;
 			}
 			else {
 				dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
 			}
-			if (dp[i][j]>0) {
+			if (result < dp[i][j]) {
 				result = dp[i][j];
 			}
 		}
+		if (temp > result) {
+			result = temp;
+		}
 	}
 
-	cout << result;
+	printf("%d", result);
 
-	
+	for (int i = 0; i < y + 2; i++) {
+		delete[] dp[i];
+	}
+	delete[]dp;
 
-	return 0;
 }
