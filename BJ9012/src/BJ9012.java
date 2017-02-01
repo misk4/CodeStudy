b39d00a69d0de2ebfbddff117f36f5d9e67f08b7
misk4 <misk4@naver.com>
2017-02-01 12:34:19 +0900

    bj9012

---
+++ BJ9012/src/BJ9012.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class BJ9012 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		Stack<Integer> stack = new Stack<Integer>();
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int testCase = Integer.parseInt(in.readLine().trim());
+		while(testCase-->0){
+			String s = in.readLine();
+			stack.clear();
+			boolean check = true;
+			for(int i=0;i<s.length();i++){
+				if(s.charAt(i)=='('){
+					stack.push(0);
+				}else{
+					if(stack.isEmpty()){
+						check = false;
+						break;
+					}
+					stack.pop();
+				}
+			}
+			if(stack.isEmpty() && check){
+				System.out.println("YES");
+			}
+			else{
+				System.out.println("NO");
+			}
+		}
+	}
+
+}
